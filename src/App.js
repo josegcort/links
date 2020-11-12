c433d83b6b3ffde33dcb7f097ac8fe9d81b6eef7
josegcort <josegcort@gmail.com>
2020-11-12 17:57:51 -0500

    Original

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import imagenes from './images.js';
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container-img">
+        <br/>
+            <img className="Img-logo" src={imagenes[0].img} alt="logo" />
+            <h4>@ccelencuentro</h4>
+            <h5>#EncuentroEnLinea</h5>
+        </div>
+        <div className="column">
+          <a href={imagenes[1].url} target="_blank">
+            <button type="button" >
+            <h2><FontAwesomeIcon icon={imagenes[1].icon} /> {imagenes[1].title}</h2>
+            </button>
+          </a>
+          <br/>
+          <a href={imagenes[2].url} target="_blank">
+            <button type="button" >
+            <h2><FontAwesomeIcon icon={imagenes[2].icon} /> {imagenes[2].title}</h2>
+            </button>
+          </a>
+          <br/>
+          <a href={imagenes[3].url} target="_blank">
+            <button type="button" >
+            <h2><FontAwesomeIcon icon={imagenes[3].icon} /> {imagenes[3].title}</h2>
+            </button>
+          </a>
+          <br/>
+          <a href={imagenes[4].url} target="_blank">
+            <button type="button" >
+            <h2><FontAwesomeIcon icon={imagenes[4].icon} /> {imagenes[4].title}</h2>
+            </button>
+          </a>
+          <br/>
+          <a href={imagenes[5].url} target="_blank">
+            <button type="button" >
+            <h2><FontAwesomeIcon icon={imagenes[5].icon} /> {imagenes[5].title}</h2>
+            </button>
+          </a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/images.js
@@ -0,0 +1,45 @@
+import logoEncuentro from './assets/img/logo.png';
+import logoYoutube from './assets/img/youtube.png';
+import logoFacebook from './assets/img/facebook.png';
+import logoInstagram from './assets/img/instagram.png';
+import logoWhatsapp from './assets/img/whatsapp.png';
+
+import { faYoutube, faFacebookF, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+
+export default[
+	{
+		"title": "Logo del Encuentro",
+		"url": "",
+		"img": logoEncuentro,
+	},
+	{
+		"title": "Canal de Youtube",
+		"url": "https://www.youtube.com/channel/UCfGWV-1TcTy-egaFGSYmboA",
+		"img": logoYoutube,
+		"icon":faYoutube
+	},
+	{
+		"title": "Fan page Facebook",
+		"url": "https://facebook.com/ccelencuentro/",
+		"img": logoFacebook,
+		"icon": faFacebookF
+	},
+	{
+		"title": "Perfil de Instagram",
+		"url": "https://www.instagram.com/elencuentroiglesiacristiana/",
+		"img": logoInstagram,
+		"icon": faInstagram
+	},
+	{
+		"title": "Chat de WhatsApp#1 ",
+		"url": "https://wa.link/t50imm",
+		"img": logoWhatsapp,
+		"icon": faWhatsapp
+	},
+	{
+		"title": "Chat de WhatsApp#2 ",
+		"url": "https://wa.link/w9nl8p",
+		"img": logoWhatsapp,
+		"icon": faWhatsapp
+	}
+]
