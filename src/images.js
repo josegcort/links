f207c89b0254c58ce9b827d8f95b2bda6cd7ed65
JoseGCort <70188828+josegcort@users.noreply.github.com>
2021-01-26 19:30:17 -0500

    Etiquetas

--- src/images.js
+++ src/images.js
@@ -13,31 +13,31 @@ var imagenes = [
 		"img": logoEncuentro,
 	},
 	{
-		"title": "Canal de Youtube",
+		"title": "YouTube",
 		"url": "https://www.youtube.com/channel/UCfGWV-1TcTy-egaFGSYmboA",
 		"img": logoYoutube,
 		"icon":faYoutube
 	},
 	{
-		"title": "Fan page Facebook",
+		"title": "Facebook",
 		"url": "https://facebook.com/ccelencuentro/",
 		"img": logoFacebook,
 		"icon": faFacebookF
 	},
 	{
-		"title": "Perfil de Instagram",
+		"title": "Instagram",
 		"url": "https://www.instagram.com/elencuentroiglesiacristiana/",
 		"img": logoInstagram,
 		"icon": faInstagram
 	},
 	{
-		"title": "Chat de WhatsApp",
+		"title": "WhatsApp",
 		"url": "https://wa.link/t50imm",
 		"img": logoWhatsapp,
 		"icon": faWhatsapp
 	},
 	{
-		"title": "Chat de WhatsApp",
+		"title": "WhatsApp",
 		"url": "https://wa.link/w9nl8p",
 		"img": logoWhatsapp,
 		"icon": faWhatsapp
